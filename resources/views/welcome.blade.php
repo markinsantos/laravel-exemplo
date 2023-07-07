4333ccfa4175f0468c11d644b00cbdc941990053
markinSantos <marcosdosantosilva@gmail.com>
2023-07-07 09:02:37 -0300

    branch de teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,8 @@
             }
         </style>
     </head>
+
+    <h1>ola</h1>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
